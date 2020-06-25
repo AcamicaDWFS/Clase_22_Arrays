13bcf979e5092fad7ba1a600037fc0edd24fe00c
celara91 <62780873+celara91@users.noreply.github.com>
2020-06-25 01:10:53 -0500

    Add files via upload

---
+++ Challengue_22/ch_22.js
@@ -0,0 +1,9 @@
+//Creación del array
+var frutas = ["piña", "melon", "naranja", "plátano", "durazno", "sandía", "guayaba", "papaya"];
+console.log(frutas);
+
+var i = 0; 
+while (i < frutas.length) {
+    console.log(frutas[i]);
+    i++;
+}
