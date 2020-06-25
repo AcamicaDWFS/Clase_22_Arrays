c99f0269c4d73dbf89f1d78a27706fc6ba358280
fannygabriela.becerra <fannygabriela.becerra@bbva.com>
2020-06-25 18:50:57 -0500

    Frutitas agregadas :3

---
+++ Challengue_22/Fanny_Gabriela_Becerra_Hernandez/main.js
@@ -0,0 +1,9 @@
+let fruitsArray = new Array("Aguacate", "Berenjena", "Pepino", "Tomate");
+let index = 0;
+
+function fruits() {
+    while (index < fruitsArray.length) {
+        console.log(fruitsArray[index]);
+        index++;
+    }
+}
