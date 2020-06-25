8ad38e7992f9ae963cf34deeab57a5feae37e08c
lisvsz <64239458+lisvsz@users.noreply.github.com>
2020-06-25 15:56:28 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//Definir variables
+let frutas = ["durazno", "piña", "mandarina", "uva", "fresa", "carambola"];
+let i = 0;
+
+//Mostrar el listado con while
+while (i < frutas.length){
+    document.write(frutas[i] + "<br>"); i++;
+}
