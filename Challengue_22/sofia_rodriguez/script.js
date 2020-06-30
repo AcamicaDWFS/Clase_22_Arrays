e783bb0f44cbc48693bd384c8821c004ec1ccea3
Sofia <sofiarodriguez2995@gmail.com>
2020-06-30 18:52:07 -0500

    sofia_rodriguez
    array frutas ciclo while

---
+++ Challengue_22/sofia_rodriguez/script.js
@@ -0,0 +1,11 @@
+function arrayFrutas () {
+
+    let frutas = ["manzana" , "platano" , "naranja" , "uva" , "pera"];
+    let i=0;
+    let frutasLength = frutas.length;
+
+    while (i < frutasLength) {
+    console.log(frutas[i]);
+    i++;
+    }
+}
